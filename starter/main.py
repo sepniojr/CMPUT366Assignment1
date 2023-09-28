73fc246b5f5587cda6f677fbbe8f62207669fb09
sepniojr <sepniojr@ualberta.ca>
2023-09-28 14:04:38 -0600

    CLEAN

--- starter/main.py
+++ starter/main.py
@@ -9,7 +9,6 @@ import heapq
 def dijkstra(start, goal, gridded_map):
     # Initialize the OPEN and CLOSED lists
     # OPEN uses heap, CLOSED uses dictionaries
-    print("Function Dijkstra")
     open_list = []
     closed_list = {}
     num_expansions = 0
@@ -18,13 +17,11 @@ def dijkstra(start, goal, gridded_map):
     heapq.heappush(open_list, start)
     closed_list[start.state_hash()] = start
     
-    #print(closed_list)
 
     while len(open_list) != 0: # While open list not empty
         # Pop the node with the smallest cost
         parent = heapq.heappop(open_list)
 
-        #closed_list[n.state_hash()] = n
         if parent == goal:
             return parent.get_cost(), num_expansions
         
@@ -37,7 +34,6 @@ def dijkstra(start, goal, gridded_map):
             hash_value = child.state_hash()
             # If this node was not encountered before
             if hash_value not in closed_list:
-                #print("Adding child " + str(hash_value) + " to open and closed list")
                 # The child's cost should be the g value generated
                 child.set_cost(child.get_g())
                 closed_list[hash_value] = child
@@ -46,38 +42,14 @@ def dijkstra(start, goal, gridded_map):
             # If this node is in closed list but the one we found is cheaper
             if hash_value in closed_list and (child.get_g() < closed_list[hash_value].get_cost()):
 
-                # Change cost of child in open list
-
-                # Update cost of this node in the closed list
+                # Update cost of this node
                 closed_list[hash_value].set_cost(child.get_g())
                 heapq.heappush(open_list, child)
-                #print("Cost changed from " + str(old_cost) + " to " + str(child.get_cost()))
-
-
-        #print("Parent " + str(parent.state_hash()) + " has " + str(count) + " children")
 
     return -1, num_expansions
             
-            
-
-        
-        
-
-#While open list not empty
-#   n = open_list.pop
-#   if n = goal
-#       return path
-#   for child in n: (get children of n)
-#       if child not in closed_list:
-#           open_list.insert(child, n.get_g() (cost of parent) + (cost connecting child and parent) (not sure how to do this))
-#           closed_list[child.state_hash()] = child
-#       if child in closed_list and ( ((cost of parent) + (cost connecting child and parent)) < closed_list[child].get_g):
-#           update cost/pointers in both open and closed lists
-#           reheapify open list
-#   return failures
 
 def astar(start, goal, gridded_map):
-    print("Function Astar")
     # Initialize the OPEN and CLOSED lists
     # OPEN uses heap, CLOSED uses dictionaries
     open_list = []
@@ -88,13 +60,11 @@ def astar(start, goal, gridded_map):
     heapq.heappush(open_list, start)
     
     closed_list[start.state_hash()] = start
-    #print(closed_list)
 
     while len(open_list) != 0: # While open list not empty
         # Pop the node with the smallest cost
         parent = heapq.heappop(open_list)
 
-        #closed_list[n.state_hash()] = n
         if parent == goal:
             return parent.get_cost(), num_expansions
         
@@ -109,15 +79,15 @@ def astar(start, goal, gridded_map):
             hash_value = child.state_hash()
             h_n = hfunction(child.get_x(), goal.get_x(), child.get_y(), goal.get_y())
             
-            # In the A star algorithm, once a node is popped off the graph we know we have found the lowest cost for that node
+
             if hash_value not in closed_list:
-                #print("Adding child " + str(hash_value) + " to open and closed list")
-                # The child's cost should be the g value generated
+
+                # The child's cost should be the f value
                 child.set_cost(child.get_g() + h_n)
                 closed_list[hash_value] = child
                 heapq.heappush(open_list, child)
 
-            # If this node is in closed list but the one we found is cheaper
+            # If this node was visited but the one we found is cheaper
             if hash_value in closed_list and ((child.get_g() + h_n) < closed_list[hash_value].get_cost()):
 
                 # Change cost of child in open list
@@ -127,8 +97,6 @@ def astar(start, goal, gridded_map):
                 closed_list[hash_value].set_g(child.get_g())
 
                 heapq.heapify(open_list)
-
-        #print("Parent " + str(parent.state_hash()) + " has " + str(count) + " children")
 
     return -1, num_expansions
 
